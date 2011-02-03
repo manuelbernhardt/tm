521723b1127fd9512ae2497d9dc8f1180114dd13
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-02-03 17:09:01 +0100

    Defects controller

---
+++ app/controllers/Defects.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
+ */
+public class Defects extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+}
