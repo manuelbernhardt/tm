4c265dfd3cc0ea4f0afc7c42c9e0b1930abbfa79
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-02-03 17:30:24 +0100

    job to load initial data

---
+++ app/Bootstrap.java
@@ -0,0 +1,17 @@
+import models.User;
+import play.jobs.Job;
+import play.jobs.OnApplicationStart;
+import play.test.Fixtures;
+
+/**
+ * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
+ */
+@OnApplicationStart
+public class Bootstrap extends Job {
+    @Override
+    public void doJob() throws Exception {
+        if(User.count() == 0) {
+            Fixtures.load("initial-data.yml");
+        }
+    }
+}
