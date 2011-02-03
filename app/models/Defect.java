df9657530232ee2f31ff95bb7a142ff43392bd01
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-02-04 00:09:35 +0100

    Correcting relationship mapping

--- app/models/Defect.java
+++ app/models/Defect.java
@@ -3,6 +3,8 @@ package models;
 import play.db.jpa.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 /**
  * @author: Gwenael Alizon <gwenael.alizon@oxiras.com>
@@ -11,8 +13,14 @@ import javax.persistence.Entity;
 public class Defect extends Model {
 
     public String title;
+
+    @ManyToOne
     public User assignedTo;
+
+    @ManyToOne
     public User submittedBy;
+
+    @OneToOne
     public DefectStatus status;
 
 }
