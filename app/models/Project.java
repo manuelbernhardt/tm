bf8c6de4af844d007b2f51d211ef0698dd7b3989
Gwenael Alizon <gwenael.alizon@oxiras.com>
2011-02-04 15:57:17 +0100

    Project model update

--- app/models/Project.java
+++ app/models/Project.java
@@ -3,6 +3,7 @@ package models;
 import play.db.jpa.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 /**
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
@@ -10,8 +11,10 @@ import javax.persistence.Entity;
 @Entity
 public class Project extends Model {
 
+    @ManyToOne
     public Account account;
 
+    @ManyToOne
     public Product product;
 
     public String name;
