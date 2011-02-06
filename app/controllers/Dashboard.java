a3513ef878b82d40525af65578286251f16c4a19
Gwenael Alizon <gwenael.alizon@oxiras.com>
2011-02-06 01:12:17 +0100

    Renaming Application into Dashboard

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -2,7 +2,7 @@ package controllers;
 
 import play.mvc.Controller;
 
-public class Application extends Controller {
+public class Dashboard extends Controller {
 
     public static void index() {
         render();
