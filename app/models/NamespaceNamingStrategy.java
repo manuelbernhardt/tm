4eacee14a211e5191455ef54411831d5f5fe2750
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-02-25 12:46:53 +0100

    More comments to the naming strategy

--- app/models/NamespaceNamingStrategy.java
+++ app/models/NamespaceNamingStrategy.java
@@ -4,7 +4,9 @@ import org.hibernate.cfg.ImprovedNamingStrategy;
 
 /**
  * Naming strategy so that we use qualified names for tables.
- * 
+ * <br />
+ * TODO this is not yet active!
+ *
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
  */
 public class NamespaceNamingStrategy extends ImprovedNamingStrategy {
