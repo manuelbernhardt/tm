c18f968006d31786d37da8e209b5765fdbea7669
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-02-25 15:24:25 +0100

    Beginning of user profile

--- app/controllers/TMController.java
+++ app/controllers/TMController.java
@@ -3,6 +3,7 @@ package controllers;
 import controllers.deadbolt.Deadbolt;
 import models.general.Auth;
 import models.project.Project;
+import models.tm.User;
 import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
@@ -19,10 +20,30 @@ public class TMController extends Controller {
     @Before
     public static void setConnectedUser() {
         if(Security.isConnected()) {
+            // FIXME search by account, too!
             Auth a = Auth.find("byEmail", Security.connected()).first();
             renderArgs.put("firstName", a.firstName);
             renderArgs.put("lastName", a.lastName);
         }
+    }
+
+    public static User getConnectedUser() {
+        if(Security.isConnected()) {
+        // FIXME search by account, too!
+        User user = User.find("from User u where u.authentication.email = ?", Security.connected()).first();
+        return user;
+        } else {
+            // TODO test this!
+            flash.put("url", "GET".equals(request.method) ? request.url : "/");
+            try {
+                Secure.login();
+            } catch (Throwable throwable) {
+                // TODO
+                throwable.printStackTrace();
+            }
+
+        }
+        return null;
     }
 
     /**
--- app/controllers/profile/Profile.java
+++ app/controllers/profile/Profile.java
@@ -2,15 +2,25 @@ package controllers.profile;
 
 import controllers.TMController;
 import controllers.deadbolt.Deadbolt;
+import models.tm.User;
 import play.mvc.With;
 
 /**
+ * TODO make sure the edited user == the authenticated user
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
  */
 @With(Deadbolt.class)
 public class Profile extends TMController {
 
     public static void index() {
-        render();
+        User user = getConnectedUser();
+        render(user);
     }
+
+    public static void editUser(User user) {
+        // TODO validation
+        user.save();
+    }
+
+
 }
