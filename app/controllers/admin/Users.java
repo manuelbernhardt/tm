a51c30ed67e9207b6fe02d66fb8ed00aae5a9350
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-02-28 16:39:37 +0100

    Nicer routes, uniform head menu

--- app/controllers/admin/Users.java
+++ app/controllers/admin/Users.java
@@ -2,16 +2,16 @@ package controllers.admin;
 
 import java.util.List;
 
+import controllers.TMController;
 import controllers.deadbolt.Deadbolt;
 import models.general.Auth;
-import play.mvc.Controller;
 import play.mvc.With;
 
 /**
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
  */
 @With(Deadbolt.class)
-public class Users extends Controller {
+public class Users extends TMController {
 
     public static void index() {
         List<Auth> users = Auth.findAll();
