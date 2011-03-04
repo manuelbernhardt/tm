236f5e91f609428e79b0b414ca8de9b2422a005a
Gwenael Alizon <gwenael.alizon@oxiras.com>
2011-03-04 19:15:59 +0100

    New set of unit roles

--- app/models/general/UnitRole.java
+++ app/models/general/UnitRole.java
@@ -8,6 +8,39 @@ public class UnitRole implements models.deadbolt.Role {
     public static final String USER = "user";
     public static final String ADMIN = "admin";
 
+    public static final String USERCREATE = "userCreate";
+    public static final String USEREDIT = "userEdit";
+    public static final String USERDELETE = "userDelete";
+
+    public static final String PROJECTCREATE = "projectCreate";
+    public static final String PROJECTEDIT = "projectEdit";
+    public static final String PROJECTDELETE = "projectDelete";
+
+    public static final String REQVIEW = "reqView";
+    public static final String REQCREATE = "reqCreate";
+    public static final String REQEDIT = "reqEdit";
+    public static final String REQDELETE = "reqDelete";
+
+    public static final String APPROACHVIEW = "approachView";
+    public static final String APPROACHCREATE = "approachCreate";
+    public static final String APPROACHEDIT = "approachEdit";
+    public static final String APPROACHDELETE = "approachDelete";
+
+    public static final String TESTPREPVIEW = "testPrepView";
+    public static final String TESTPREPCREATE = "testPrepCreate";
+    public static final String TESTPREPEDIT = "testPrepEdit";
+    public static final String TESTPREPDELETE = "testPrepDelete";
+
+    public static final String TESTEXECVIEW = "testExecView";
+    public static final String TESTEXECCREATE = "testExecCreate";
+    public static final String TESTEXECEDIT = "testExecEdit";
+    public static final String TESTEXECDELETE = "testExecDelete";
+
+    public static final String DEFECTVIEW = "defectView";
+    public static final String DEFECTCREATE = "defectCreate";
+    public static final String DEFECTEDIT = "defectEdit";
+    public static final String DEFECTDELETE = "defectDelete";
+
     private String name;
 
     public String getRoleName() {
@@ -24,7 +57,7 @@ public class UnitRole implements models.deadbolt.Role {
 
     @Override
     public boolean equals(Object o) {
-        return o instanceof UnitRole && ((UnitRole)o).getRoleName().equals(this.getRoleName());
+        return o instanceof UnitRole && ((UnitRole) o).getRoleName().equals(this.getRoleName());
     }
 
     @Override
