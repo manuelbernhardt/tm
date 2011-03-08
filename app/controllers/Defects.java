4b2f62ec3b8c3dacc9363dad3ea1e65eb0df3b77
Gwenael Alizon <gwenael.alizon@oxiras.com>
2011-03-08 10:32:37 +0100

    Initial revision for defect listing per project

--- app/controllers/Defects.java
+++ app/controllers/Defects.java
@@ -2,7 +2,10 @@ package controllers;
 
 import java.util.List;
 
+import controllers.tabularasa.TableController;
 import models.project.Defect;
+import models.project.Project;
+import play.db.jpa.GenericModel;
 
 /**
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
@@ -10,8 +13,24 @@ import models.project.Defect;
 public class Defects extends TMController {
 
     public static void index() {
-        List<Defect> defects = Defect.findAll();
-        render(defects);
+        render();
+    }
+
+    public static void data(String tableId,
+                            Integer iDisplayStart,
+                            Integer iDisplayLength,
+                            String sColumns,
+                            String sEcho) {
+
+        Project activeProject = getActiveProject();
+
+        GenericModel.JPAQuery query = null;
+        query = Defect.find("from Defect d where d.project = ?", activeProject).from(iDisplayStart == null ? 0 : iDisplayStart);
+
+        List<Defect> roles = query.fetch(iDisplayLength == null ? 10 : iDisplayLength);
+        long totalRecords = Defect.count();
+        TableController.renderJSON(roles, Defect.class, totalRecords, sColumns, sEcho);
+        ok();
     }
 
     public static void create(Defect defect) {
