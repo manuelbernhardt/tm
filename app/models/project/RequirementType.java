c37304a01683f9cf2b69b30af546e8cd9cae7829
Gwenael Alizon <gwenael.alizon@oxiras.com>
2011-03-15 16:13:50 +0100

    Initial model for requirements

---
+++ app/models/project/RequirementType.java
@@ -0,0 +1,17 @@
+package models.project;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ * @author: Gwenael Alizon <gwenael.alizon@oxiras.com>
+ */
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(name="id", columnNames = {"project_id", "naturalId"})})
+public class RequirementType {
+
+    public Integer position;
+    public String type;
+
+}
