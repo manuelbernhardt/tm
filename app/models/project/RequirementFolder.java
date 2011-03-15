6fd297e1e71ff2ef2e09c6021ebc25153a145056
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-03-15 18:42:34 +0100

    Minor model adjustments

--- app/models/project/RequirementFolder.java
+++ app/models/project/RequirementFolder.java
@@ -1,14 +1,17 @@
 package models.project;
 
-import play.data.validation.Required;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import play.data.validation.Required;
 
 /**
  * @author: Gwenael Alizon <gwenael.alizon@oxiras.com>
  */
 @Entity
+@Table(uniqueConstraints = {@UniqueConstraint(name = "id", columnNames = {"project_id", "naturalId"})})
 public class RequirementFolder extends ProjectModel {
 
     @Column(nullable = false)
--- app/models/project/RequirementType.java
+++ app/models/project/RequirementType.java
@@ -8,8 +8,8 @@ import javax.persistence.UniqueConstraint;
  * @author: Gwenael Alizon <gwenael.alizon@oxiras.com>
  */
 @Entity
-@Table(uniqueConstraints = {@UniqueConstraint(name="id", columnNames = {"project_id", "naturalId"})})
-public class RequirementType {
+@Table(uniqueConstraints = {@UniqueConstraint(name = "id", columnNames = {"project_id", "naturalId"})})
+public class RequirementType extends ProjectModel {
 
     public Integer position;
     public String type;
