21786cf78146783484c4762bb57b7f8a8ba45a81
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-03-17 16:29:20 +0100

    Post-processing of Fixtures (self-referring entities not yet supported by Play)

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,4 +1,7 @@
+import java.util.List;
+
 import models.tm.User;
+import models.tree.jpa.TreeNode;
 import play.jobs.Job;
 import play.jobs.OnApplicationStart;
 import play.test.Fixtures;
@@ -12,6 +15,13 @@ public class Bootstrap extends Job {
     public void doJob() throws Exception {
         if(User.count() == 0) {
             Fixtures.loadModels("initial-data.yml");
+
+            // fix the treeNodes
+            List<TreeNode> rootNodes = TreeNode.find("from TreeNode n where n.threadRoot is null").fetch();
+            for(TreeNode n : rootNodes) {
+                n.threadRoot = n;
+                n.save();
+            }
         }
     }
 }
