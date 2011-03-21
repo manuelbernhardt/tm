6365c14643fed4a70e372bd4bbbf837df96e7ca5
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-03-21 18:28:23 +0100

    More on the instance tabs

--- app/controllers/preparation/Instances.java
+++ app/controllers/preparation/Instances.java
@@ -12,25 +12,28 @@ public class Instances extends TMController {
         Instance instance = getInstance(instanceId);
         render(instance);
     }
-    
+
     public static void tags(Long instanceId) {
-      
+        Instance instance = getInstance(instanceId);
+        render(instance);
     }
-    
+
     public static void schedule(Long instanceId) {
-      
+        Instance instance = getInstance(instanceId);
+        render(instance);
     }
-    
+
     public static void testData(Long instanceId) {
-      
+        Instance instance = getInstance(instanceId);
+        render(instance);
     }
 
     private static Instance getInstance(Long instanceId) {
-        if(instanceId == null) {
+        if (instanceId == null) {
             return null;
         }
         Instance instance = Instance.<Instance>findById(instanceId);
-        if(instance == null) {
+        if (instance == null) {
             return null;
         }
         checkInAccount(instance);
