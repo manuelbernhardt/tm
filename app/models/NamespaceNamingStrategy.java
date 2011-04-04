189a488ca922ccb1b99158c403203602a9d07bbd
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-04-04 08:08:19 +0200

    Strategy that would work, except for HHH-4312

--- app/models/NamespaceNamingStrategy.java
+++ app/models/NamespaceNamingStrategy.java
@@ -1,19 +1,22 @@
 package models;
 
-import org.hibernate.cfg.ImprovedNamingStrategy;
+import org.hibernate.cfg.EJB3NamingStrategy;
 
 /**
  * Naming strategy so that we use qualified names for tables.
  * <br />
- * TODO this is not yet active!
+ * TODO does not work yet due to http://opensource.atlassian.com/projects/hibernate/browse/HHH-4312
  *
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
  */
-public class NamespaceNamingStrategy extends ImprovedNamingStrategy {
+public class NamespaceNamingStrategy extends EJB3NamingStrategy {
+
+    public NamespaceNamingStrategy() {
+
+    }
 
     @Override
     public String classToTableName(String className) {
-        // cut off "models"
-        return addUnderscores(className.substring(7));
+        return className.indexOf("models.") > -1 ? className.substring("models.".length()).replaceAll("\\.", "_") : className.replaceAll("\\.", "_");
     }
 }
