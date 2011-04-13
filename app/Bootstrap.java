b2235f979122a52cb0efda4b0b98bea3d8a6340e
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-04-13 14:49:54 +0200

    Fixing naturalID

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,7 +1,11 @@
 import java.util.List;
 
+import models.SchemaCreation;
 import models.tm.User;
 import models.tree.jpa.TreeNode;
+import org.hibernate.Session;
+import play.Play;
+import play.db.jpa.JPA;
 import play.jobs.Job;
 import play.jobs.OnApplicationStart;
 import play.test.Fixtures;
@@ -9,18 +13,29 @@ import play.test.Fixtures;
 /**
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
  */
-@OnApplicationStart
+@OnApplicationStart()
 public class Bootstrap extends Job {
     @Override
     public void doJob() throws Exception {
-        if(User.count() == 0) {
-            Fixtures.loadModels("initial-data.yml");
 
-            // fix the treeNodes
-            List<TreeNode> rootNodes = TreeNode.find("from TreeNode n where n.threadRoot is null").fetch();
-            for(TreeNode n : rootNodes) {
-                n.threadRoot = n;
-                n.save();
+        // this is a Bootstrap job for the development mode, making it easy to re-create data and schema elements out of nowhere
+        // for production, we'll need to export the schema creation into a consolidated script
+        if (Play.mode == Play.Mode.DEV) {
+            if (User.count() == 0) {
+
+                // for development: create triggers && validate model
+                SchemaCreation schemaCreation = new SchemaCreation(((Session) JPA.em().getDelegate()).getSessionFactory());
+                schemaCreation.validateCompositeModels();
+                schemaCreation.createTriggers();
+
+                Fixtures.loadModels("initial-data.yml");
+
+                // fix the treeNodes
+                List<TreeNode> rootNodes = TreeNode.find("from TreeNode n where n.threadRoot is null").fetch();
+                for (TreeNode n : rootNodes) {
+                    n.threadRoot = n;
+                    n.save();
+                }
             }
         }
     }
--- app/models/general/CompositeModel.java
+++ app/models/general/CompositeModel.java
@@ -2,20 +2,19 @@ package models.general;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.PrePersist;
 
 /**
+ * A composite model, which has a "natural" identifier. The naturalIdentifier mechanism is implemented by means of MySQL triggers.
+ *
+ * @see models.SchemaCreation
+ *
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
  */
+
 @MappedSuperclass
 public class CompositeModel extends TemporalModel {
 
-    @Column(nullable = false)
+    @Column(insertable = false, updatable = false, columnDefinition = "bigint(20) NOT NULL DEFAULT -1")
     public Long naturalId;
-
-    @PrePersist
-    public void prePersist() {
-        naturalId = CompositeModelIdentifierGenerator.getNextNaturalIdentifier(this.getClass());
-    }
 
 }
