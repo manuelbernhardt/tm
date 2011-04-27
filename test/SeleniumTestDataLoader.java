bb30fba6115c03d3bfd9d57002eec84ab89ce899
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-04-27 14:40:33 +0200

    Changing the way the test data is being loaded

---
+++ test/SeleniumTestDataLoader.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+import models.tm.ProjectTreeNode;
+import models.tree.jpa.TreeNode;
+import play.test.Fixtures;
+
+/**
+ * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
+ */
+public class SeleniumTestDataLoader {
+
+    public SeleniumTestDataLoader() {
+        Fixtures.deleteDatabase();
+        Fixtures.loadModels("selenium/testData.yml");
+
+                // fix the treeNodes
+                List<ProjectTreeNode> rootNodes = TreeNode.find("from ProjectTreeNode n where n.threadRoot is null").fetch();
+                for (ProjectTreeNode n : rootNodes) {
+                    n.threadRoot = n;
+                    n.save();
+                }
+    }
+}
