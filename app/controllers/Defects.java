9983f5cfa53daab4a875727bd4024d51702df1b3
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-05-09 16:15:07 +0200

    Clearing

--- app/controllers/Defects.java
+++ app/controllers/Defects.java
@@ -1,48 +1,8 @@
 package controllers;
 
-import java.util.List;
-
-import controllers.deadbolt.Restrict;
-import controllers.tabularasa.TableController;
-import models.general.UnitRole;
-import models.tm.Defect;
-import models.tm.Project;
-import play.db.jpa.GenericModel;
-
 /**
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
  */
 public class Defects extends TMController {
-
-    @Restrict(UnitRole.DEFECTVIEW)
-    public static void index() {
-        render();
-    }
-
-    @Restrict(UnitRole.DEFECTVIEW)
-    public static void data(String tableId,
-                            Integer iDisplayStart,
-                            Integer iDisplayLength,
-                            String sColumns,
-                            String sEcho) {
-
-        Project activeProject = getActiveProject();
-
-        GenericModel.JPAQuery query = null;
-        query = Defect.find("from Defect d where d.project = ?", activeProject).from(iDisplayStart == null ? 0 : iDisplayStart);
-
-        List<Defect> roles = query.fetch(iDisplayLength == null ? 10 : iDisplayLength);
-        long totalRecords = Defect.count();
-        TableController.renderJSON(roles, Defect.class, totalRecords, sColumns, sEcho);
-        ok();
-    }
-
-    @Restrict(UnitRole.DEFECTCREATE)
-    public static void create(Defect defect) {
-        defect.project = getActiveProject();
-        defect.submittedBy = getConnectedUser();
-        defect.create();
-        index();
-    }
 
 }
