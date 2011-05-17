8fa6a8bcb816aaf426946e1c8edf3ea53c1a99f4
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-05-17 09:43:43 +0200

    Making the script nicer

--- setup.sh
+++ setup.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-echo ================
+echo ===================================
 echo Cleaning modules and work directory
-echo ================
+echo ===================================
 rm -rf modules
 mkdir modules
 play clean
 
 echo
 echo
-echo ================
+echo ======================
 echo Resolving dependencies
-echo ================
+echo ======================
 echo
 echo
 play dependencies --sync
 
 echo
 echo
-echo ================
+echo =============================
 echo Installing additional modules
-echo ================
+echo =============================
 echo
 echo
 git clone https://github.com/manuelbernhardt/tree modules/tree
@@ -29,10 +29,12 @@ cd modules/play-excel && git checkout with-lib && cd ../..
 
 echo
 echo
-echo ================
+echo =========================================================================
 echo Done! Visit http://oxiras.localhost.com:9000
-echo (note that you must have added oxiras.localhost to your /etc/hosts file)
-echo ================
+echo
+echo Note that you must have added the following line to your /etc/hosts file:
+echo 127.0.0.1 oxiras.localhost
+echo =========================================================================
 echo
 echo
 
