f9fd122350cc64440211d28eda4ae38f5a0b3844
Nikola Milivojevic <0dziga0@gmail.com>
2011-05-28 17:08:20 +0200

    renaming tags

--- app/controllers/admin/Projects.java
+++ app/controllers/admin/Projects.java
@@ -57,31 +57,10 @@ public class Projects extends TMController {
 
     @Restrict(UnitRole.ACCOUNTADMIN)
     public static void tags(Long projectId){
-//        List<Tag> tags = Tag.find("select t from Tag t where t.project.id = ?", projectId).fetch();
-//        List<Tag> tagsRequirement = new ArrayList<Tag>();
-//        List<Tag> tagsTestScript = new ArrayList<Tag>();
-//        List<Tag> tagsTestInstance = new ArrayList<Tag>();
-//        List<Tag> tagsDefect = new ArrayList<Tag>();
-//
-//        for(Tag t:tags){
-//            if(t.type==Tag.TagType.REQUIREMENT){
-//                tagsRequirement.add(t);
-//            }
-//            else if(t.type==Tag.TagType.TESTSCRIPT){
-//                tagsTestScript.add(t);
-//            }
-//            else if(t.type==Tag.TagType.TESTINSTANCE){
-//                tagsTestInstance.add(t);
-//            }
-//            else if(t.type==Tag.TagType.DEFECT){
-//                tagsDefect.add(t);
-//            }
-//        }
-
-//        render(tagsRequirement, tagsTestScript, tagsTestInstance,tagsDefect, projectId);
         render(projectId);
     }
 
+    @Restrict(UnitRole.ACCOUNTADMIN)
     public static void tagsData(String tableId,
                                 Integer tagType,
                             Integer iDisplayStart,
@@ -117,6 +96,17 @@ public class Projects extends TMController {
             long totalRecords = Tag.count();
             TableController.renderJSON(tags, Tag.class, totalRecords, sColumns, sEcho);
             ok();
+        }
+    }
+
+    public static void renameTag(Long tagId, String tagNewName, Long projectId){
+        List<Tag> tags = Tag.find("select t from Tag t where t.id=? and t.project.id=?", tagId, projectId).fetch();
+        if(tags.size()>1){
+            
+        }
+        else if(tags.size()==1){
+            tags.get(0).name = tagNewName;
+            tags.get(0).save();
         }
     }
 
