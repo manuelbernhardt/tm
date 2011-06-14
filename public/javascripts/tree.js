13aa558409b69f2b98cb77042eeb87dd785cfc91
Nikola Milivojevic <0dziga0@gmail.com>
2011-06-14 12:12:22 +0200

    refactoring projects -> cycles, small fix

--- public/javascripts/tree.js
+++ public/javascripts/tree.js
@@ -126,7 +126,7 @@
                                     $.jstree.rollback(data.rlbk);
                                 } else {
                                     if(typeof options.onRename === 'function') {
-                                        options.onRename.call(data.rslt.new_name, data.rslt.new_name);
+                                        options.onRename.call(data.rslt.new_name, data.rslt.new_name, data.rslt.obj.attr("rel"));
                                     }
                                 }
                             }
