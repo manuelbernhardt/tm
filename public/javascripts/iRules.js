d4266e265120f8ae18e14dec2e0dc7e2a9af6b6a
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-06-29 19:58:55 +0200

    Rulifying the whole requirement page

--- public/javascripts/iRules.js
+++ public/javascripts/iRules.js
@@ -11,8 +11,8 @@ function iRule(eventSourceSelector, eventType, conditionClosure, targetSelector,
 
     $(eventSourceSelector).bind(eventType, function(event, data) {
 
-        // TODO check if the closure is valid
-        if (conditionClosure(event, data)) {
+        var call = (typeof conditionClosure === 'function' && conditionClosure(event, data)) || (typeof conditionClosure === 'boolean' && conditionClosure) || conditionClosure == null
+        if (call) {
             // TODO check if the closure is valid
             actionClosure($(targetSelector), data, event);
         }
