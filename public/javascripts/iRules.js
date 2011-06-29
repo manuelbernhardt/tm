eebb31be8fdb842ce4f4f2622bb8b98f0db30213
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2011-06-29 19:25:54 +0200

    Starting iRules

---
+++ public/javascripts/iRules.js
@@ -0,0 +1,35 @@
+var data = $(document).data('iRules');
+
+if (typeof data === 'undefined') {
+    data = {
+            interactionGroups: {}
+        };
+    $(document).data('iRules', data);
+}
+
+function iRule(eventSourceSelector, eventType, conditionClosure, targetSelector, actionClosure) {
+
+    $(eventSourceSelector).bind(eventType, function(event, data) {
+
+        // TODO check if the closure is valid
+        if (conditionClosure(event, data)) {
+            // TODO check if the closure is valid
+            actionClosure($(targetSelector), data, event);
+        }
+    });
+}
+
+function iGroupRule(groupName, targetSelector, actionClosure) {
+    // TODO check if the group exists
+    var group = data.interactionGroups[groupName];
+    iRule(group.eventSourceSelector, group.eventType, group.conditionClosure, targetSelector, actionClosure);
+}
+
+function iGroup(groupName, eventSourceSelector, eventType, conditionClosure) {
+    data.interactionGroups[groupName] = {
+        eventSourceSelector: eventSourceSelector,
+        eventType: eventType,
+        conditionClosure: conditionClosure
+    }
+}
+
