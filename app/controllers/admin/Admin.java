ed1b71a4b269de23c7c808eb2f57b156086e4dc0
Nikola Milivojevic <0dziga0@gmail.com>
2011-07-20 13:31:06 +0200

    adding feedback bar to admin area

--- app/controllers/admin/Admin.java
+++ app/controllers/admin/Admin.java
@@ -2,6 +2,7 @@ package controllers.admin;
 
 import com.sun.servicetag.UnauthorizedAccessException;
 import controllers.TMController;
+import notifiers.TMMails;
 import play.mvc.results.Unauthorized;
 import util.Logger;
 
@@ -9,19 +10,21 @@ import util.Logger;
  * @author Nikola Milivojevic
  */
 public class Admin extends TMController {
-    public static void index(){
-        if(TMController.isUserSuperAdmin()){
+    public static void index() {
+        if (TMController.isUserSuperAdmin()) {
             Users.index();
-        }
-        else if(TMController.isProjectSuperAdmin()){
+        } else if (TMController.isProjectSuperAdmin()) {
             Projects.index();
-        }
-        else if (TMController.isAccountAdmin()) {
+        } else if (TMController.isAccountAdmin()) {
             AccountSettings.index();
-        }
-        else{
+        } else {
             Logger.error(Logger.LogType.SECURITY, "Unauthorized user tried to access admin area. User id " + getConnectedUserId());
             forbidden();
         }
+    }
+
+    public static void sendFeedbackEmail(String name, String message, String location) {
+        if (play.Play.id.equals("demo"))
+            TMMails.feedbackEmail(name, message, location, request.headers.toString());
     }
 }
