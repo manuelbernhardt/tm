e0a22089f14829d2969091c870f88c27de45851c
Nikola Milivojevic <0dziga0@gmail.com>
2011-08-26 16:09:01 +0200

    adding tree and jquery cookie plugin, related to #126

--- public/javascripts/tree.js
+++ public/javascripts/tree.js
@@ -13,7 +13,7 @@
         var settings = {};
 
         var defaults = {
-            "plugins" : [ "json_data", "ui", "types", "themeroller", "search"],
+            "plugins" : [ "json_data", "ui", "types", "themeroller", "search", "cookies"],
             "json_data" : {
                 "ajax" : {
                     "url" : treeChildrenRouteAction(),
@@ -37,6 +37,10 @@
             "search": {
                 "case_insensitive": true,
                 "show_only_matches": true
+            },
+            "cookies" : {
+                "auto_save" : true,
+                "save_opened" : false
             }
         };
 
@@ -46,8 +50,8 @@
                 "move": {
                     "always_copy": "multitree",
                     "check_move" : function(m) {
-                        var canMove = !hasNodeWithSameName(m.rt,m.np,m.o);
-                        if(!canMove) {
+                        var canMove = !hasNodeWithSameName(m.rt, m.np, m.o);
+                        if (!canMove) {
                             errorMessage("The object with the same name already exists in the folder. Rename the object before moving.");
                         }
                         return canMove;
@@ -56,22 +60,22 @@
             },
             "hotkeys": {
                 "Ctrl+x" : function() {
-                    if(treeNodeCanEdit) {
+                    if (treeNodeCanEdit) {
                         this.cut(null)
                     }
                 },
                 "Ctrl+v" : function() {
-                    if(treeNodeCanEdit) {
+                    if (treeNodeCanEdit) {
                         this.paste(null)
                     }
                 },
                 "f2" : function() {
-                    if(treeNodeCanEdit) {
+                    if (treeNodeCanEdit) {
                         this.rename(null)
                     }
                 },
                 "Del" : function() {
-                    if(treeNodeCanDelete) {
+                    if (treeNodeCanDelete) {
                         this.remove(null)
                     }
                 }
@@ -80,7 +84,7 @@
             "contextmenu": {
                 "items" :  function(node) {
                     var items = {};
-                    if(treeNodeCanCreate) {
+                    if (treeNodeCanCreate) {
                         $.extend(items, {
                             "create": {
                                 "label": "Create",
@@ -91,7 +95,7 @@
                             }
                         });
                     }
-                    if(treeNodeCanEdit) {
+                    if (treeNodeCanEdit) {
                         $.extend(items, {
                             "rename": {
                                 "label": "Rename",
@@ -120,7 +124,7 @@
                             }
                         });
                     }
-                    if(treeNodeCanDelete) {
+                    if (treeNodeCanDelete) {
                         $.extend(items, {
                             "delete": {
                                 "label": "Delete",
@@ -145,7 +149,7 @@
             if (options.plugins) {
                 settings.plugins = $.merge(options.plugins, defaults.plugins);
             }
-            if(options.types.multipurpose) {
+            if (options.types.multipurpose) {
                 $.extend(settings, multiPurpose);
                 settings.plugins = $.merge(settings.plugins, multiPurposePlugins);
             }
@@ -154,79 +158,79 @@
         var treeId = this.attr("id");
 
         this.jstree(settings).bind("create.jstree",
-                function (e, data) {
-                    $.post(treeCreateRouteAction(),
+            function (e, data) {
+                $.post(treeCreateRouteAction(),
+                {
+                    "treeId" : treeId,
+                    "parentId": data.rslt.parent.attr ? extractId(data.rslt.parent.attr("id")) : -1,
+                    "parentType": data.rslt.parent.attr ? data.rslt.parent.attr("rel") : "",
+                    "position" : data.rslt.position,
+                    "name" : data.rslt.name,
+                    "type" : data.rslt.obj.attr("rel"),
+                    "args" : data.rslt.obj.data("args") ? data.rslt.obj.data("args") : null
+
+                },
+                    function (r) {
+                        if (r.status) {
+                            $(data.rslt.obj).attr("id", "node_" + r.rel + "_" + r.id);
+                            $(data.rslt.obj).attr("rel", r.rel);
+                        }
+                        else {
+                            $.jstree.rollback(data.rlbk);
+                        }
+                    }
+                    );
+            }).bind("delete_node.jstree",
+            function (e, data) {
+                data.rslt.obj.each(function () {
+                    $.ajax({
+                        async : false,
+                        type: 'DELETE',
+                        url: treeRemoveRouteAction(),
+                        data : {
+                            "treeId" : treeId,
+                            "id" : extractId(this.id),
+                            "parentId": data.rslt.parent.attr ? extractId(data.rslt.parent.attr("id")) : -1,
+                            "type" : data.rslt.obj.attr("rel"),
+                            "args" : data.rslt.obj.data("args") ? data.rslt.obj.data("args") : null
+
+                        },
+                        success : function (r) {
+                            if (!r.status) {
+                                if (typeof options.onDeleteFailed === 'function') {
+                                    options.onDeleteFailed.call();
+                                }
+                                data.inst.refresh();
+                            }
+                        }
+                    });
+                });
+            }).bind("rename.jstree",
+            function (e, data) {
+                var canRename = !hasNodeWithSameName(data.inst, data.inst._get_parent(data.rslt.obj), data.rslt.obj);
+                if (canRename) {
+                    $.post(treeRenameRouteAction(),
                     {
                         "treeId" : treeId,
-                        "parentId": data.rslt.parent.attr ? extractId(data.rslt.parent.attr("id")) : -1,
-                        "parentType": data.rslt.parent.attr ? data.rslt.parent.attr("rel") : "",
-                        "position" : data.rslt.position,
-                        "name" : data.rslt.name,
-                        "type" : data.rslt.obj.attr("rel"),
-                        "args" : data.rslt.obj.data("args") ? data.rslt.obj.data("args") : null
-
+                        "id" : extractId(data.rslt.obj.attr("id")),
+                        "name" : data.rslt.new_name,
+                        "type" : data.rslt.obj.attr("rel")
                     },
-                            function (r) {
-                                if (r.status) {
-                                    $(data.rslt.obj).attr("id", "node_" + r.rel + "_" + r.id);
-                                    $(data.rslt.obj).attr("rel", r.rel);
-                                }
-                                else {
-                                    $.jstree.rollback(data.rlbk);
-                                }
-                            }
-                            );
-                }).bind("delete_node.jstree",
-                function (e, data) {
-                    data.rslt.obj.each(function () {
-                        $.ajax({
-                            async : false,
-                            type: 'DELETE',
-                            url: treeRemoveRouteAction(),
-                            data : {
-                                "treeId" : treeId,
-                                "id" : extractId(this.id),
-                                "parentId": data.rslt.parent.attr ? extractId(data.rslt.parent.attr("id")) : -1,
-                                "type" : data.rslt.obj.attr("rel"),
-                                "args" : data.rslt.obj.data("args") ? data.rslt.obj.data("args") : null
-
-                            },
-                            success : function (r) {
-                                if (!r.status) {
-                                    if(typeof options.onDeleteFailed === 'function') {
-                                        options.onDeleteFailed.call();
-                                    }
-                                    data.inst.refresh();
+                        function (r) {
+                            if (!r.status) {
+                                $.jstree.rollback(data.rlbk);
+                            } else {
+                                if (typeof options.onRename === 'function') {
+                                    options.onRename.call(data.rslt.new_name, data.rslt.new_name, data.rslt.obj.attr("rel"));
                                 }
                             }
-                        });
-                    });
-                }).bind("rename.jstree",
-                function (e, data) {
-                    var canRename = !hasNodeWithSameName(data.inst,data.inst._get_parent(data.rslt.obj),data.rslt.obj);
-                    if(canRename){
-                        $.post(treeRenameRouteAction(),
-                        {
-                            "treeId" : treeId,
-                            "id" : extractId(data.rslt.obj.attr("id")),
-                            "name" : data.rslt.new_name,
-                            "type" : data.rslt.obj.attr("rel")
-                        },
-                                function (r) {
-                                    if (!r.status) {
-                                        $.jstree.rollback(data.rlbk);
-                                    } else {
-                                        if(typeof options.onRename === 'function') {
-                                            options.onRename.call(data.rslt.new_name, data.rslt.new_name, data.rslt.obj.attr("rel"));
-                                        }
-                                    }
-                                }
-                                );
-                    }else{
-                        errorMessage("Another object with the same name already exists in the folder. Rename one of the objects first.");
-                        $.jstree.rollback(data.rlbk);
-                    }
-                }).bind("move_node.jstree", function (e, data) {
+                        }
+                        );
+                } else {
+                    errorMessage("Another object with the same name already exists in the folder. Rename one of the objects first.");
+                    $.jstree.rollback(data.rlbk);
+                }
+            }).bind("move_node.jstree", function (e, data) {
             data.rslt.o.each(function (i) {
                 $.ajax({
                     async : false,
